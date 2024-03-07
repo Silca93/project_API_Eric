e5964dc705dd7497aadf256a37beb8553e71158a
Silca93 <eric_delhaye@.com>
2024-03-07 12:03:30 +0100

    project creation

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useRef, useEffect } from 'react'
+import { useContext } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="main w-dvw h-dvh bg-red-400">
+      
+    </div>
+  )
+}
+
+export default App
